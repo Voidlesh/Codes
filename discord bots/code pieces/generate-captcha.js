f8a1f017054d847fb811e531d6c3828760bb3d67
Voidlesh <98331236+Voidlesh@users.noreply.github.com>
2022-06-11 17:55:11 +0200

    Update generate-captcha.js

--- discord bots/code pieces/generate-captcha.js
+++ discord bots/code pieces/generate-captcha.js
@@ -1,11 +1,10 @@
 function genCode() {
   let codes = [];
-  let str =
-    "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890!@#$%'";
+  let string = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890!@#$%'"; // string with all avaiable letters
   for (let i = 0; i < 6; i++) {
-    codes.push(str[Math.floor(Math.random() * str.length)]);
+    codes.push(string[Math.floor(Math.random() * string.length)]); // choose random letters
   }
-  return codes.join("");
+  return codes.join(""); // join al l random letters
 }
 
-console.log(genCode());
+console.log(genCode()); // log random code in console

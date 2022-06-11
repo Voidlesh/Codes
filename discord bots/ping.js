66c26a3f2e1e24a6d46434e6ddae0b1da69a5ee5
Voidlesh <98331236+Voidlesh@users.noreply.github.com>
2022-06-11 17:49:00 +0200

    Update ping.js

--- discord bots/ping.js
+++ discord bots/ping.js
@@ -1,10 +1,16 @@
 const Discord = require("discord.js")
 const prefix = "!"
+
 client.on("messageCreate", async message => {
-  if(message.channel.type == "DM") return //bot will not read dm commands
-   if(message.content == `${prefix}ping`){
-    const ping = new Discord.MessageEmbed()
+  if(message.channel.type == "DM") return // cancel if messsage is a dm
+  
+   if(message.content == `${prefix}ping`){ // command
+     
+    const ping = new Discord.MessageEmbed() // creating embed
     .setColor("GREEN")
     .setTitle(`Bot Ping :- ${client.ws.ping}`)
+    
+    message.channel.send({ embeds: [ ping ] }) // sending embed
+    
   }
 })
